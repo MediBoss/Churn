69eaa3a5f6e3bd0ecd642b96ca1b4a7333e88215
Medi Assumani <mediassumani49@gmail.com>
2019-12-01 16:09:46 -0800

    [Add] starter express barebone

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 
-var express = require("express")
+const express = require("express")
+const { ApolloServer, gql } = require('apollo-server-express')
+
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
