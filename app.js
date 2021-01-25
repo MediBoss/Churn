67c5c26524e08facff1c2f470e5df24ce7cef3f3
Medi Assumani <mediassumani49@gmail.com>
2021-01-24 19:54:30 -0500

    [Add] basic schema and resolver

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 
-const express = require("express")
-const { ApolloServer, gql } = require('apollo-server-express')
+// Import required libraries
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 
-
+// Create an instance of the express server
 const app = express();
-server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+
+const schema = gql `
+  type Query {
+    med: User
+  }
+
+  type User {
+    username: String!
+  }
+`
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: "MediBoss"
+      }
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+// Using applyMiddleware to to set our express app as our middleware
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
