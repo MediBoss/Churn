74069f3e45a239d001f6955a98bb65d95612f57c
Medi Assumani <mediassumani49@gmail.com>
2021-02-20 19:02:10 -0500

    resolvers??

--- app.js
+++ app.js
@@ -3,26 +3,49 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
+// Cors is needed to make HTTP requests to other domains
+import cors from 'cors';
+
 // Create an instance of the express server
 const app = express();
+app.use(cors());
 
-
+// NOTE: The exclamation point after ID and String means they're non-nullable values.
+// NOTE: The ID data type is an identifier used for caching & fetching.
+// NOTE: "type" here is like "class" in Swift.
 const schema = gql `
   type Query {
-    med: User
+    me: User
+    user(id: ID!): User
   }
 
   type User {
+    id: ID!
     username: String!
   }
-`
+`;
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Medi Boss', },
+  2: {
+    id: '2',
+    username: 'Dave Davids', },
+};
+
+const me = users[1];
+
+// NOTE: The resolver takes care of returning data for fields from the schema.
 const resolvers = {
   Query: {
+    user: (parent, { id }) => {
+      return users[id];
+    },
+
     me: () => {
-      return {
-        username: "MediBoss"
-      }
-    }
+      return me;
+    },
   }
 }
 
